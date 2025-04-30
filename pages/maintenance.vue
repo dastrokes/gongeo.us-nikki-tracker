ce21cbd5895e3c00bed98764a923c993241c8c24
dastrokes <dastrokes.infinity.nikki@gmail.com>
2025-04-29 22:43:15 -0400

    UI spacing fixes

--- pages/maintenance.vue
+++ pages/maintenance.vue
@@ -1,27 +1,39 @@
+<script setup lang="ts">
+  import { Discord } from '@vicons/fa'
+</script>
+
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-100">
-    <div class="max-w-md w-full p-8 bg-white rounded-lg shadow-lg text-center">
-      <div class="mb-8">
-        <svg
-          class="w-24 h-24 mx-auto text-gray-400"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-          />
-        </svg>
+  <div class="max-w-7xl mx-auto space-y-4">
+    <n-card
+      class="rounded-xl"
+      no-title
+    >
+      <div class="text-center mb-12">
+        <h1 class="text-2xl font-bold mb-4">Under Maintenance</h1>
+        <p class="text-gray-600">
+          We're currently performing some maintenance on our site to improve
+          your experience.
+        </p>
+        <p class="text-gray-600">
+          We'll be back online shortly. Thank you for your patience!
+        </p>
       </div>
-      <h1 class="text-3xl font-bold text-gray-800 mb-4">Under Maintenance</h1>
-      <p class="text-gray-600 mb-6">
-        We're currently performing some maintenance on our site to improve your
-        experience. We'll be back online shortly. Thank you for your patience!
-      </p>
-    </div>
+
+      <n-space
+        justify="center"
+        class="mt-12"
+      >
+        <n-button
+          tag="a"
+          href="https://discord.gg/K92RAEvCXx"
+          target="_blank"
+        >
+          <template #icon>
+            <n-icon><Discord /></n-icon>
+          </template>
+          Join Discord
+        </n-button>
+      </n-space>
+    </n-card>
   </div>
 </template>

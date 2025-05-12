b19ad9b7b263348bfe50fe48b9ccfba507db20c0
dastrokes <dastrokes.infinity.nikki@gmail.com>
2025-05-12 11:57:39 -0400

    fix build

--- composables/useImageProvider.ts
+++ composables/useImageProvider.ts
@@ -1,13 +1,8 @@
 import { computed } from 'vue'
 
 export const useImageProvider = () => {
-  const config = useRuntimeConfig()
-
   const imageProvider = computed(() => {
-    return config.public.siteUrl?.includes('gongeo.us') ||
-      config.public.siteUrl?.includes('netlify')
-      ? 'netlify'
-      : 'ipx'
+    return process.env.NODE_ENV === 'development' ? 'ipx' : 'netlify'
   })
 
   return {

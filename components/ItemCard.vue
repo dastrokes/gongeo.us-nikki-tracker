79c22ae1ee7971d6f47d52faa17b623132064fa5
dastrokes <dastrokes.infinity.nikki@gmail.com>
2025-05-12 10:20:49 -0400

    fix image for cn

--- components/ItemCard.vue
+++ components/ItemCard.vue
@@ -107,6 +107,8 @@
   })
 
   const imageProvider = computed(() => {
-    return process.env.NODE_ENV === 'development' ? 'ipx' : 'netlify'
+    return process.env.NUXT_PUBLIC_SITE_URL === 'https://gongeo.us'
+      ? 'netlify'
+      : 'ipx'
   })
 </script>

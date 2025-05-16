59a25cf723b1db7f7b7eb17353334ecb73d6d223
dastrokes <hnzzzm@gmail.com>
2025-05-16 10:34:38 -0400

    add more toggle

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -5,10 +5,10 @@
     @select="handleLanguageSelect"
   >
     <n-button
+      :aria-label="t('accessibility.language_switcher')"
       text
       size="tiny"
       class="flex items-center"
-      aria-label="Language switcher"
     >
       <n-icon>
         <Language />
@@ -21,7 +21,7 @@
   import { computed } from 'vue'
   import { Language } from '@vicons/fa'
 
-  const { locales, setLocale } = useI18n()
+  const { locales, setLocale, t } = useI18n()
 
   const languageOptions = computed(() => {
     return (locales.value as { code: string; name: string }[]).map((l) => ({

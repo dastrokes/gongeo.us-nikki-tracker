65800ea197f40e912bdacc77f59606e3e63b72a1
dastrokes <hnzzzm@gmail.com>
2025-05-20 15:26:23 -0400

    add maintenance for import page

--- middleware/maintenance.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  const config = useRuntimeConfig()
-  const isMaintenanceMode = config.public.maintenance
-  // Allow the maintenance page itself to be accessed
-  if (isMaintenanceMode && to.path !== '/maintenance') {
-    return navigateTo('/maintenance')
-  }
-})

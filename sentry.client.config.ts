6a0b6786f9057be8ae9aa526ac90b3c86db57662
dastrokes <dastrokes.infinity.nikki@gmail.com>
2025-06-11 23:09:33 -0400

    update sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/nuxt'
 Sentry.init({
   dsn: 'https://80cb15ee9f3c92eaa8cdcee97a75425e@o4509482068869120.ingest.us.sentry.io/4509482070376448',
   tracesSampleRate: 0.1,
-  replaysSessionSampleRate: 0.01,
+  replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
   integrations: [Sentry.replayIntegration()],
   debug: false,

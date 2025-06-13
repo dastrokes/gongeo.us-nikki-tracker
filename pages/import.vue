73b7e78ae02dcc1abcd81012d159f7ad96f8d47b
dastrokes <hnzzzm@gmail.com>
2025-06-12 21:12:18 -0400

    v1.6 data update

--- pages/import.vue
+++ pages/import.vue
@@ -610,7 +610,7 @@
 
   const { t } = useI18n()
   const dialog = useDialog()
-  const isMaintenanceMode = true
+  const isMaintenanceMode = false
   const router = useRouter()
   const localePath = useLocalePath()
   const siteUrl = useRuntimeConfig().public.siteUrl

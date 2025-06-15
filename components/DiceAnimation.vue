5c3782dfa0413321ea6b52774c10fc984512dfe2
dastrokes <dastrokes.infinity.nikki@gmail.com>
2025-06-14 22:05:04 -0400

    permanent banner stats

--- components/DiceAnimation.vue
+++ components/DiceAnimation.vue
@@ -8,7 +8,7 @@
         <n-button
           text
           circle
-          class="text-gray-500 hover:text-gray-700"
+          class="text-gray-500"
         >
           <n-icon
             class="origin-center"

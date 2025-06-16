36156cfe92081d9b9bbf78bd1c3a48e2f20625b0
dastrokes <dastrokes.infinity.nikki@gmail.com>
2025-06-16 18:49:03 -0400

    update umami domain

--- layouts/default.vue
+++ layouts/default.vue
@@ -302,7 +302,7 @@
         async: true,
         defer: true,
         src: '/gongeous.js',
-        'data-host-url': 'https://gongeous-umami.netlify.app',
+        'data-host-url': 'https://gongeous-api.netlify.app',
         'data-website-id': 'dd22ab5d-2045-4450-aaff-f513339b5ca6',
       },
     ],

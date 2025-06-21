e74d7ca3452f3231734477eb77f3d3c4c739441e
dastrokes <hnzzzm@gmail.com>
2025-06-21 00:58:39 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -142,6 +142,18 @@ export default defineNuxtConfig({
     },
   },
 
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      cssnano: process.env.NODE_ENV === 'production'
+        ? {
+            preset: ['default', { discardComments: { removeAll: true } }],
+          }
+        : false, // disable cssnano when not in production
+    },
+  },
+  
   sentry: {
     sourceMapsUploadOptions: {
       org: 'dastrokes',

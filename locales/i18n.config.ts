4912ec609c72ca866db1d7cddb90d56d66a2cb77
dastrokes <dastrokes.infinity.nikki@gmail.com>
2025-06-25 11:35:24 -0400

    fix language

--- locales/i18n.config.ts
+++ locales/i18n.config.ts
@@ -1,25 +1,4 @@
-export const defaultLocale = 'en'
-
-export const i18nLocales = [
-  {
-    code: 'en',
-    iso: 'en-US',
-    name: 'English',
-    files: ['en/common.json', 'en/outfit.json'],
-  },
-  {
-    code: 'de',
-    iso: 'de-DE',
-    name: 'Deutsch',
-    files: ['de/common.json', 'de/outfit.json'],
-  },
-  {
-    code: 'zh',
-    iso: 'zh-CN',
-    name: '中文',
-    files: ['zh/common.json', 'zh/outfit.json'],
-  },
-]
+import { defaultLocale } from './locales'
 
 export default defineI18nConfig(() => ({
   fallbackLocale: defaultLocale,

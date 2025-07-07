5ddd1bd3807b3311c025b9588de38c5d9dc52dc4
dastrokes <dastrokes.infinity.nikki@gmail.com>
2025-07-06 23:22:20 -0400

    update sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,8 +1,10 @@
-import * as Sentry from '@sentry/nuxt'
+import { init } from '@sentry/nuxt'
 
-Sentry.init({
+init({
   dsn: 'https://80cb15ee9f3c92eaa8cdcee97a75425e@o4509482068869120.ingest.us.sentry.io/4509482070376448',
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0.01,
+  maxBreadcrumbs: 10,
+
   debug: false,
   enabled: process.env.NODE_ENV === 'production',
 })

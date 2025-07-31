97e745c91906a79b50a0e2ff5dfba78f29662271
dastrokes <hnzzzm@gmail.com>
2025-07-30 22:19:33 -0400

    use node function

--- server/utils/hash.ts
+++ server/utils/hash.ts
@@ -1,7 +1,5 @@
+import { createHash } from 'crypto'
+
 export async function hashUid(uid: string): Promise<string> {
-  const encoder = new TextEncoder()
-  const data = encoder.encode(uid)
-  const hashBuffer = await globalThis.crypto.subtle.digest('SHA-256', data)
-  const hashArray = Array.from(new Uint8Array(hashBuffer))
-  return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+  return createHash('sha256').update(uid).digest('hex')
 }

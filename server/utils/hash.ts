eed95a4ac57a69ee26beab1377a22aaa5ee5c848
dastrokes <dastrokes.infinity.nikki@gmail.com>
2025-07-30 22:07:01 -0400

    use globalThis for hash

--- server/utils/hash.ts
+++ server/utils/hash.ts
@@ -1,7 +1,7 @@
 export async function hashUid(uid: string): Promise<string> {
   const encoder = new TextEncoder()
   const data = encoder.encode(uid)
-  const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+  const hashBuffer = await globalThis.crypto.subtle.digest('SHA-256', data)
   const hashArray = Array.from(new Uint8Array(hashBuffer))
   return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
 }

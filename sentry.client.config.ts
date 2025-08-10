3da215d3467595476d07d0e5bf250b34e9fb7e5f
dastrokes <hnzzzm@gmail.com>
2025-08-10 13:19:00 -0400

    disable replay

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,10 +3,11 @@ import * as Sentry from '@sentry/nuxt'
 Sentry.init({
   dsn: 'https://80cb15ee9f3c92eaa8cdcee97a75425e@o4509482068869120.ingest.us.sentry.io/4509482070376448',
   tracesSampleRate: 0.1,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  integrations: [Sentry.replayIntegration()],
   maxBreadcrumbs: 20,
+
+  // replaysSessionSampleRate: 0.1,
+  // replaysOnErrorSampleRate: 1.0,
+  // integrations: [Sentry.replayIntegration()],
 
   beforeSend(event) {
     const ignorePatterns = [

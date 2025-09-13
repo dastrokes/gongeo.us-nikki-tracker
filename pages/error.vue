00d6d4cf5a586aede28581020f422f1b9c49b968
dastrokes <hnzzzm@gmail.com>
2025-09-12 21:58:01 -0400

    add error page

--- pages/error.vue
+++ pages/error.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="min-h-screen flex items-center justify-center">
     <n-result
-      size="huge"
+      size="small"
       status="404"
-      :title="t('error.defaultTitle')"
-      :description="t('error.defaultMessage')"
-      :sub-title="t('error.notFound')"
+      :title="t('error.404')"
+      :description="t('error.notFound')"
     >
+      <template #icon>
+        <NuxtImg
+          src="/images/404.webp"
+          alt="404"
+          class="w-2/3 h-2/3 object-cover"
+          format="webp"
+          width="400"
+          height="400"
+          fit="cover"
+          :quality="100"
+          sizes="400px sm:400px"
+        />
+      </template>
       <template #footer>
         <n-button
           round

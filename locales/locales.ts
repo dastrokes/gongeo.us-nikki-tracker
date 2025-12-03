92a4f9330732ae6b6e8cfff66b9f9f63f41e5dc3
dastrokes <hnzzzm@gmail.com>
2025-12-03 00:18:32 -0500

    add languages

--- locales/locales.ts
+++ locales/locales.ts
@@ -1,6 +1,18 @@
 import type { LocaleObject } from '@nuxtjs/i18n'
 
-export type SupportedLocaleCode = 'en' | 'de' | 'zh' | 'ko'
+export type SupportedLocaleCode =
+  | 'en'
+  | 'de'
+  | 'es'
+  | 'fr'
+  | 'id'
+  | 'it'
+  | 'ja'
+  | 'ko'
+  | 'pt'
+  | 'th'
+  | 'tw'
+  | 'zh'
 
 export const defaultLocale: SupportedLocaleCode = 'en'
 
@@ -28,14 +40,58 @@ export const i18nLocales: LocaleObject<SupportedLocaleCode>[] = [
     ],
   },
   {
-    code: 'zh',
-    iso: 'zh-CN',
-    name: '中文',
+    code: 'es',
+    iso: 'es-ES',
+    name: 'Español',
     files: [
-      'zh/common.json',
-      'zh/outfit.json',
-      'zh/item.json',
-      'zh/version.json',
+      'es/common.json',
+      'es/outfit.json',
+      'es/item.json',
+      'es/version.json',
+    ],
+  },
+  {
+    code: 'fr',
+    iso: 'fr-FR',
+    name: 'Français',
+    files: [
+      'fr/common.json',
+      'fr/outfit.json',
+      'fr/item.json',
+      'fr/version.json',
+    ],
+  },
+  {
+    code: 'id',
+    iso: 'id-ID',
+    name: 'Bahasa Indonesia',
+    files: [
+      'id/common.json',
+      'id/outfit.json',
+      'id/item.json',
+      'id/version.json',
+    ],
+  },
+  {
+    code: 'it',
+    iso: 'it-IT',
+    name: 'Italiano',
+    files: [
+      'it/common.json',
+      'it/outfit.json',
+      'it/item.json',
+      'it/version.json',
+    ],
+  },
+  {
+    code: 'ja',
+    iso: 'ja-JP',
+    name: '日本語',
+    files: [
+      'ja/common.json',
+      'ja/outfit.json',
+      'ja/item.json',
+      'ja/version.json',
     ],
   },
   {
@@ -47,6 +103,50 @@ export const i18nLocales: LocaleObject<SupportedLocaleCode>[] = [
       'ko/outfit.json',
       'ko/item.json',
       'ko/version.json',
+    ],
+  },
+  {
+    code: 'pt',
+    iso: 'pt-PT',
+    name: 'Português',
+    files: [
+      'pt/common.json',
+      'pt/outfit.json',
+      'pt/item.json',
+      'pt/version.json',
+    ],
+  },
+  {
+    code: 'th',
+    iso: 'th-TH',
+    name: 'ไทย',
+    files: [
+      'th/common.json',
+      'th/outfit.json',
+      'th/item.json',
+      'th/version.json',
+    ],
+  },
+  {
+    code: 'tw',
+    iso: 'zh-TW',
+    name: '繁體中文',
+    files: [
+      'tw/common.json',
+      'tw/outfit.json',
+      'tw/item.json',
+      'tw/version.json',
+    ],
+  },
+  {
+    code: 'zh',
+    iso: 'zh-CN',
+    name: '中文',
+    files: [
+      'zh/common.json',
+      'zh/outfit.json',
+      'zh/item.json',
+      'zh/version.json',
     ],
   },
 ]

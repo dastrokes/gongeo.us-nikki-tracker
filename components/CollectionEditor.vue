7efccc4aed7e31578302d38ba4fe93b301dd417c
dastrokes <hnzzzm@gmail.com>
2026-01-05 23:18:06 -0500

    update route banners

--- components/CollectionEditor.vue
+++ components/CollectionEditor.vue
@@ -54,11 +54,12 @@
                     :src="`/images/items/icons/${itemId}.png`"
                     :alt="t(`item.${itemId}.name`)"
                     class="w-full aspect-square object-cover rounded-md"
+                    preset="iconLg"
                     width="120"
                     height="120"
                     fit="cover"
                     loading="lazy"
-                    placeholder="/images/loading.webp"
+                    placeholder="/loading.webp"
                     sizes="80px sm:120px"
                   />
                 </template>

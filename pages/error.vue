d0dff56b9b9827f487ae17c902d759527a932ea4
dastrokes <hnzzzm@gmail.com>
2026-01-27 17:04:07 -0500

    fix prerender

--- pages/error.vue
+++ pages/error.vue
@@ -1,13 +1,3 @@
 <template>
   <ErrorNotFound />
 </template>
-
-<script setup lang="ts">
-  import { setResponseStatus } from 'h3'
-
-  const event = useRequestEvent()
-
-  if (import.meta.server && event) {
-    setResponseStatus(event, 404)
-  }
-</script>

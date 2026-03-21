5dca655a8e3c26439dc1d8da959c3b7b9207b37c
dastrokes <hnzzzm@gmail.com>
2026-03-21 09:40:11 -0400

    google adsense

--- app/pages/[...all].vue
+++ app/pages/[...all].vue
@@ -3,10 +3,6 @@
 </template>
 
 <script setup lang="ts">
-  definePageMeta({
-    umami: false,
-  })
-
   const event = useRequestEvent()
   if (import.meta.server && event) {
     setResponseStatus(event, 404)

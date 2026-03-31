f1530918cddc76eb6f0ebcb517b819ff7f12e6a2
dastrokes <hnzzzm@gmail.com>
2026-03-30 21:55:12 -0400

    enable search

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -95,6 +95,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     supabaseSecretKey: process.env.SUPABASE_SECRET_KEY,
     supabaseDataSecretKey: process.env.SUPABASE_DATA_SECRET_KEY,
+    upstashVectorRestUrl: process.env.UPSTASH_VECTOR_REST_URL,
+    upstashVectorRestToken: process.env.UPSTASH_VECTOR_REST_TOKEN,
 
     public: {
       supabaseUrl: process.env.SUPABASE_DATABASE_URL,

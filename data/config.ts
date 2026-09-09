5a420acde3639987c898ca8bb5686887ab6ec6b2
dastrokes <hnzzzm@gmail.com>
2026-09-09 16:06:32 -0400

    maintenance on

--- data/config.ts
+++ data/config.ts
@@ -2,7 +2,7 @@ import { BANNER_DATA } from './banners'
 
 export const LATEST_BANNER_ID = 73
 
-export const IMPORT_PAGE_MAINTENANCE = false
+export const IMPORT_PAGE_MAINTENANCE = true
 
 const limitedBannerRuns = Object.values(BANNER_DATA).flatMap((banner) =>
   banner.bannerType === 1
